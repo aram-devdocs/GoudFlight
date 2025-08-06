19363bb359ce4204686df244dcd4427c06874e43
Aram Hammoudeh <aram.devdocs@gmail.com>
2025-08-06 14:29:15 -0600

    Update platformio.ini and launch.json for ESP32-S3 support; modify handheld_config.h for new pin mappings and remove unused profiles

--- lib/Config/handheld_config.h
+++ lib/Config/handheld_config.h
@@ -3,14 +3,19 @@
 
 #include "hardware_profiles.h"
 
-static const hardware_profile_t handheld_v1_profile = {
+// ESP32-S3-N16R8 Pin Mapping
+// Available GPIOs: 1-21, 33-48
+// Avoid: GPIO 19-20 (USB), GPIO 26-32 (SPI Flash)
+// I2C recommended: GPIO 8 (SDA), GPIO 9 (SCL)
+
+static const hardware_profile_t handheld_profile = {
     .header = {
         .magic_number = HAL_CONFIG_MAGIC,
         .version = HAL_CONFIG_VERSION,
         .board_type = HAL_BOARD_HANDHELD
     },
     .profile_id = HW_PROFILE_HANDHELD_V1,
-    .board_name = "Handheld Controller V1",
+    .board_name = "Handheld Controller",
     .display = {
         .driver_type = DISPLAY_DRIVER_SSD1306,
         .params = {
@@ -19,8 +24,8 @@ static const hardware_profile_t handheld_v1_profile = {
                 .height = 64,
                 .i2c_address = 0x3C,
                 .reset_pin = -1,
-                .sda_pin = 21,
-                .scl_pin = 22
+                .sda_pin = 8,  // ESP32-S3 I2C SDA
+                .scl_pin = 9   // ESP32-S3 I2C SCL
             }
         }
     },
@@ -28,9 +33,9 @@ static const hardware_profile_t handheld_v1_profile = {
         .driver_type = INPUT_DRIVER_SHIFT_REGISTER,
         .params = {
             .shift_register = {
-                .load_pin = 25,
-                .clock_pin = 26,
-                .data_pin = 27,
+                .load_pin = 10,  // ESP32-S3 GPIO
+                .clock_pin = 11, // ESP32-S3 GPIO
+                .data_pin = 12,  // ESP32-S3 GPIO
                 .num_registers = 1,
                 .clock_delay_us = 5,
                 .load_delay_us = 5
@@ -44,51 +49,6 @@ static const hardware_profile_t handheld_v1_profile = {
     }
 };
 
-static const hardware_profile_t handheld_v2_profile = {
-    .header = {
-        .magic_number = HAL_CONFIG_MAGIC,
-        .version = HAL_CONFIG_VERSION,
-        .board_type = HAL_BOARD_HANDHELD
-    },
-    .profile_id = HW_PROFILE_HANDHELD_V2,
-    .board_name = "Handheld Controller V2",
-    .display = {
-        .driver_type = DISPLAY_DRIVER_SSD1306,
-        .params = {
-            .ssd1306 = {
-                .width = 128,
-                .height = 64,
-                .i2c_address = 0x3D,
-                .reset_pin = 4,
-                .sda_pin = 21,
-                .scl_pin = 22
-            }
-        }
-    },
-    .input = {
-        .driver_type = INPUT_DRIVER_SHIFT_REGISTER,
-        .params = {
-            .shift_register = {
-                .load_pin = 32,
-                .clock_pin = 33,
-                .data_pin = 34,
-                .num_registers = 2,
-                .clock_delay_us = 3,
-                .load_delay_us = 3
-            }
-        }
-    },
-    .resource_limits = {
-        .max_stack_bytes = 4096,
-        .max_heap_bytes = 16384,
-        .max_cpu_percent = 75
-    }
-};
-
-#if defined(HARDWARE_VERSION_V2)
-    #define HANDHELD_HARDWARE_PROFILE handheld_v2_profile
-#else
-    #define HANDHELD_HARDWARE_PROFILE handheld_v1_profile
-#endif
+#define HANDHELD_HARDWARE_PROFILE handheld_profile
 
 #endif
